12536a4b7a619f26ffc0a149a98fbca83020c807
julio-natividade <julioc9@gmail.com>
2026-03-27 16:57:02 -0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const siteConfig = {
     // ===== IDENTIFICAÇÃO =====
     siteName: "Top Service",
-    logoUrl: "LOGO-4.png", // Substitua pela URL da sua logo real
+    logoUrl: "LOGO-4-TRA.png", // Substitua pela URL da sua logo real
     
     // ===== CONTATOS =====
     phone: "0800-718-8787",
